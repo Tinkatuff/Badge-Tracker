15747d2c7373cbc3cc5905371ffaeb2f88ab378a
Nikki DelRosso <nikki@resoundcreative.com>
2017-06-28 19:33:38 -0700

    Updated seed and placeholder about page

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -3,17 +3,7 @@
 @section('page-title', 'About The League')
 
 @section('content')
-	<div class="container" id="challengers">
-		<div class="row">
-			<div class="col-xs-6 col-sm-4">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent malesuada elit mauris, quis condimentum mauris interdum vitae. Cras quis arcu tortor. Nam lectus leo, viverra eu porta in, ornare a magna. Nam neque nisi, auctor non bibendum sit amet, mollis ut risus. Nunc sit amet sagittis dolor. Etiam interdum faucibus tincidunt. Suspendisse vitae leo sed orci convallis mattis a at enim. Curabitur rhoncus velit leo, quis aliquam nulla pellentesque in.
-			</div>
-			<div class="col-xs-6 col-sm-4">
-				stuff that goes on <b>this page</b>
-			</div>
-			<div class="col-xs-6 col-sm-4">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent malesuada elit mauris, quis condimentum mauris interdum vitae. Cras quis arcu tortor. Nam lectus leo, viverra eu porta in, ornare a magna. Nam neque nisi, auctor non bibendum sit amet, mollis ut risus. Nunc sit amet sagittis dolor. Etiam interdum faucibus tincidunt. Suspendisse vitae leo sed orci convallis mattis a at enim. Curabitur rhoncus velit leo, quis aliquam nulla pellentesque in.
-			</div>
-		</div>
+	<div class="container">
+		Someday this will be an about page!
 	</div>
 @endsection
